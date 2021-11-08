658bffccf262a9b0c0d34ce9309f4ca108891115
thuanphat2009 <anhxanh2019@gmail.com>
2021-11-08 10:51:41 +0700

    music left

--- js.js
+++ js.js
@@ -5,13 +5,15 @@ const song = $$('.opcity-song')
 const listSong = $('.content-right-playlist')
 const listMainMenu = $('.menu-main')
 const newPlayList = $('.menu-playlist')
-console.log(listMainMenu)
+const music = $('#music')
+console.log(music)
 
 song.forEach(function(value,index){
     value.onclick = function(){
-        listSong.style.height = '440px'
-        listMainMenu.style.height = '330px'
+        listSong.style.height = '452px'
+        listMainMenu.style.height = '340px'
         listMainMenu.style.overflowY = 'auto';
         newPlayList.style.display = 'none';
+        music.style.display = 'flex';
     }
 })
